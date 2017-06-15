1638a6ea5fb140eb3bb4daa0b4a617049977577a
Damien Villeneuve <d.villeneuve@beapp.fr>
2017-06-15 10:49:01 +0200

    Upgrade to Rx2

--- src/test/java/fr/beapp/bus/RxBusTest.java
+++ src/test/java/fr/beapp/bus/RxBusTest.java
@@ -1,131 +1,138 @@
 package fr.beapp.bus;
 
-import org.junit.Assert;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.TestObserver;
 import org.junit.Before;
 import org.junit.Test;
 
-import rx.functions.Action1;
-import rx.observers.TestSubscriber;
-
 public class RxBusTest {
 
-	private final RxBus rxBus = RxBus.getInstance();
-	private TestSubscriber<Integer> testSubscriber1;
-	private TestSubscriber<Integer> testSubscriber2;
+    private final RxBus rxBus = RxBus.getInstance();
+    private TestObserver<Integer> testObserver1;
+    private TestObserver<Integer> testObserver2;
 
-	@Before
-	public void initTest() {
-		testSubscriber1 = TestSubscriber.create();
-		testSubscriber2 = TestSubscriber.create();
-	}
+    @Before
+    public void initTest() {
+        testObserver1 = TestObserver.create();
+        testObserver2 = TestObserver.create();
+    }
 
-	@Test
-	public void testRegister_subscribed_noValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2);
-		rxBus.send("otherEvent");
+    @Test
+    public void testRegister_subscribed_noValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        rxBus.send("otherEvent");
 
-		testSubscriber1.assertNoValues();
-		testSubscriber2.assertNoValues();
-	}
+        testObserver1.assertNoValues();
+        testObserver2.assertNoValues();
+    }
 
-	@Test
-	public void testRegister_subscribed_oneValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2);
-		rxBus.send("otherEvent");
-		rxBus.send(1);
+    @Test
+    public void testRegister_subscribed_oneValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        rxBus.send("otherEvent");
+        rxBus.send(1);
 
-		testSubscriber1.assertValueCount(1);
-		testSubscriber1.assertValue(1);
-		testSubscriber2.assertValueCount(1);
-		testSubscriber2.assertValue(1);
-	}
+        testObserver1.assertValueCount(1);
+        testObserver1.assertValue(1);
+        testObserver2.assertValueCount(1);
+        testObserver2.assertValue(1);
+    }
 
-	@Test
-	public void testRegister_subscribed_threeValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2);
-		rxBus.send(1);
-		rxBus.send("otherEvent");
-		rxBus.send(2);
-		rxBus.send(3);
+    @Test
+    public void testRegister_subscribed_threeValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        rxBus.send(1);
+        rxBus.send("otherEvent");
+        rxBus.send(2);
+        rxBus.send(3);
 
-		testSubscriber1.assertValueCount(3);
-		testSubscriber1.assertValues(1, 2, 3);
-		testSubscriber2.assertValueCount(3);
-		testSubscriber2.assertValues(1, 2, 3);
-	}
+        testObserver1.assertValueCount(3);
+        testObserver1.assertValues(1, 2, 3);
+        testObserver2.assertValueCount(3);
+        testObserver2.assertValues(1, 2, 3);
+    }
 
-	@Test
-	public void testRegister_mixinSubscribed() {
-		TestSubscriber<String> testSubscriberString = TestSubscriber.create();
+    @Test
+    public void testRegister_mixinSubscribed() {
+        TestObserver<String> testObserverString = TestObserver.create();
 
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(String.class).subscribe(testSubscriberString);
-		rxBus.send(1);
-		rxBus.send("event1");
-		rxBus.send(2);
-		rxBus.send("event2");
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(String.class).subscribe(testObserverString);
+        rxBus.send(1);
+        rxBus.send("event1");
+        rxBus.send(2);
+        rxBus.send("event2");
 
-		testSubscriber1.assertValueCount(2);
-		testSubscriber1.assertValues(1, 2);
-		testSubscriberString.assertValueCount(2);
-		testSubscriberString.assertValues("event1", "event2");
-	}
+        testObserver1.assertValueCount(2);
+        testObserver1.assertValues(1, 2);
+        testObserverString.assertValueCount(2);
+        testObserverString.assertValues("event1", "event2");
+    }
 
-	@Test
-	public void testRegister_unsubscribed_noValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2).unsubscribe();
-		rxBus.send("otherEvent");
+    @Test
+    public void testRegister_unsubscribed_noValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        testObserver2.dispose();
+        rxBus.send("otherEvent");
 
-		testSubscriber1.assertNoValues();
-		testSubscriber2.assertNoValues();
-	}
+        testObserver1.assertNoValues();
+        testObserver2.assertNoValues();
+    }
 
-	@Test
-	public void testRegister_unsubscribed_oneValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2).unsubscribe();
-		rxBus.send("otherEvent");
-		rxBus.send(1);
+    @Test
+    public void testRegister_unsubscribed_oneValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        testObserver2.dispose();
+        rxBus.send("otherEvent");
+        rxBus.send(1);
 
-		testSubscriber1.assertValueCount(1);
-		testSubscriber1.assertValue(1);
-		testSubscriber2.assertNoValues();
-	}
+        testObserver1.assertValueCount(1);
+        testObserver1.assertValue(1);
+        testObserver2.assertNoValues();
+    }
 
-	@Test
-	public void testRegister_unsubscribed_threeValue() {
-		rxBus.register(Integer.class).subscribe(testSubscriber1);
-		rxBus.register(Integer.class).subscribe(testSubscriber2).unsubscribe();
-		rxBus.send("otherEvent");
-		rxBus.send(1);
-		rxBus.send(2);
-		rxBus.send(3);
+    @Test
+    public void testRegister_unsubscribed_threeValue() {
+        rxBus.register(Integer.class).subscribe(testObserver1);
+        rxBus.register(Integer.class).subscribe(testObserver2);
+        testObserver2.dispose();
+        rxBus.send("otherEvent");
+        rxBus.send(1);
+        rxBus.send(2);
+        rxBus.send(3);
 
-		testSubscriber1.assertValueCount(3);
-		testSubscriber1.assertValues(1, 2, 3);
-		testSubscriber2.assertNoValues();
-	}
+        testObserver1.assertValueCount(3);
+        testObserver1.assertValues(1, 2, 3);
+        testObserver2.assertNoValues();
+    }
 
-	@Test
-	public void testSameReference() {
-		TestSubscriber<DummyEvent> testReferenceSubscriber = TestSubscriber.create();
+    @Test
+    public void testSameReference() {
+        TestObserver<DummyEvent> testReferenceObservable = TestObserver.create();
 
-		rxBus.register(DummyEvent.class)
-				.doOnNext(new Action1<DummyEvent>() {
-					@Override
-					public void call(DummyEvent dummyEvent) {
-						dummyEvent.setValue(2);
-					}
-				})
-				.subscribe(testReferenceSubscriber);
-		rxBus.send(new DummyEvent(1));
+        rxBus.register(DummyEvent.class)
+                .doOnNext(new Consumer<DummyEvent>() {
+                    @Override
+                    public void accept(DummyEvent dummyEvent) throws Exception {
+                        dummyEvent.setValue(2);
+                    }
+                })
+                .subscribe(testReferenceObservable);
+        rxBus.send(new DummyEvent(1));
 
-		testReferenceSubscriber.assertValueCount(1);
-		Assert.assertEquals(2, testReferenceSubscriber.getOnNextEvents().get(0).getValue());
-	}
+        testReferenceObservable.assertValueCount(1);
+        testReferenceObservable.assertValueAt(0, new Predicate<DummyEvent>() {
+            @Override
+            public boolean test(DummyEvent dummyEvent) throws Exception {
+                return dummyEvent.getValue() == 2;
+            }
+        });
+    }
 
 }
